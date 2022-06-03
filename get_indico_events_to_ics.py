49aff07644f819307d9532e931d75914f8bc1649
David Berghaus <s6ddberg@uni-bonn.de>
2022-06-03 16:15:20 +0200

    first commit

---
+++ get_indico_events_to_ics.py
@@ -0,0 +1,110 @@
+#!/usr/bin/env python3
+
+import requests
+import json
+import os
+from datetime import datetime
+import pytz
+from icalendar import Calendar, Event
+from pathlib import Path
+import yaml
+
+try:
+    from urllib.parse import urlencode
+except ImportError:
+    from urllib import urlencode
+
+def build_indico_request(path, params, only_public=False):
+    items = list(params.items()) if hasattr(params, 'items') else list(params)
+    if only_public:
+        items.append(('onlypublic', 'yes'))
+    if not items:
+        return path
+    return '%s?%s' % (path, urlencode(items))
+
+def exec_get_request(indico_instance, request_path, api_token=None):
+    req_headers = {'Accept': 'application/json', 'Content-Type': 'application/json', 'Authorization': f'Bearer {api_token}'}
+    req = requests.get(indico_instance + request_path, headers=req_headers)
+    return req
+
+def exec_post_request(indico_instance, request_path, data, api_token=None):
+    req_headers = {'Accept': 'application/json', 'Content-Type': 'application/json', 'Authorization': f'Bearer {api_token}'}
+    req = requests.post(indico_instance + request_path, headers=req_headers, data=data)
+    return req
+
+def get_jresp(req): #Return req converted to json
+    try:
+      jresp = req.json() #We could also do json.loads(req.content) I am not sure what is more portable
+    except:
+      print("Error parsing JSON response!")
+      os.exit(1)
+    return jresp
+
+def get_room_ids(indico_instance, api_token=None): #Get dict of all rooms in indico instance with ids as keys and names as values
+    PATH = '/rooms/api/rooms'
+    PARAMS = {}
+    request_path = build_indico_request(PATH, PARAMS)
+    req = exec_get_request(indico_instance, request_path, api_token=API_TOKEN)
+    jresp = get_jresp(req)
+    room_ids = {room_data['id']:room_data['full_name'] for room_data in jresp}
+    return room_ids
+
+def get_bookings(indico_instance, room_id, start_date, end_date, api_token=None):
+    data = json.dumps({'room_ids': [room_id]})
+    PATH = '/rooms/api/calendar'
+    PARAMS = {
+        'start_date': start_date,
+        'end_date': end_date
+    }
+    request_path = build_indico_request(PATH, PARAMS)
+    req = exec_post_request(indico_instance, request_path, data, api_token=api_token)
+    jresp = get_jresp(req)
+    return list(jresp[0]['bookings'].values())
+
+def get_date_time_obj(bookings_date_time_str):
+    bookings_date_time_str_no_T = bookings_date_time_str.replace("T", " ") #The indico dates have an ugly T in the middle that I don't know how to process
+    date_time_obj = datetime.strptime(bookings_date_time_str_no_T, '%Y-%m-%d %H:%M:%S')
+    date_time_obj = pytz.utc.localize(date_time_obj) #Make object aware of timezone which we need for icalendar
+    return date_time_obj
+
+def get_event(booking):
+    event = Event()
+    event.add('dtstart', get_date_time_obj(booking['start_dt']))
+    event.add('dtend', get_date_time_obj(booking['end_dt']))
+    event.add('summary', booking['reservation']['booking_reason'])
+    event['organizer'] = booking['reservation']['booked_for_name']
+    return event
+
+def get_calendar(bookings):
+    calendar = Calendar()
+    for booking in bookings:
+        booking = booking[0] #This is ugly and we could replace it by looping over map(lambda x: x[0], bookings) but it might be more portable
+        event = get_event(booking)
+        calendar.add_component(event)
+    return calendar
+
+def save_calendar_to_file(calendar, room_id):
+    directory = Path.cwd() / 'icalendars'
+    f = open(os.path.join(directory, '{}.ics'.format(room_id)), 'wb')
+    f.write(calendar.to_ical())
+    f.close()
+
+if __name__ == '__main__':
+    with open("config.yaml", "r") as stream:
+        try:
+            config = yaml.safe_load(stream)
+        except yaml.YAMLError as exc:
+            print(exc)
+    INDICO_INSTANCE = config['indico_instance']
+    API_TOKEN = config['api_token']
+    start_date = config['start_date']
+    end_date = config['end_date']
+    room_ids = get_room_ids(INDICO_INSTANCE, api_token=API_TOKEN)
+    directory = Path.cwd() / 'icalendars'
+    directory.mkdir(parents=True, exist_ok=True)
+    with open(os.path.join(directory, 'room_id_mappings.txt'), 'w') as f:
+        for (room_id,room_name) in sorted(room_ids.items()):
+            bookings = get_bookings(INDICO_INSTANCE, room_id, start_date, end_date, api_token=API_TOKEN)
+            calendar = get_calendar(bookings)
+            save_calendar_to_file(calendar, room_id)
+            f.write('{}: {}\n'.format(room_id, room_name))
